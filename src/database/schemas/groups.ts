0c9aa08e72f5454522b3c269013b40ac5138e129
PhantomX256 <vedverma2004@gmail.com>
2026-03-13 15:09:19 +0530

    Add icon and color to groups schema

--- src/database/schemas/groups.ts
+++ src/database/schemas/groups.ts
@@ -12,6 +12,8 @@ export const groupsTable = pgTable("groups", {
 	internal_id: serial("internal_id").primaryKey(),
 	id: uuid("id").defaultRandom().notNull().unique(),
 	name: text("name").notNull(),
+	icon: text("icon").notNull(),
+	color: text("color").notNull(),
 	created_by: integer("created_by")
 		.notNull()
 		.references(() => usersTable.internal_id),
