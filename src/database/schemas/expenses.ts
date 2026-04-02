f3e229cc4ba588f5ddc0b58a75da2f552f802962
PhantomX256 <vedverma2004@gmail.com>
2026-04-02 14:51:56 +0530

    Add split mode field

--- src/database/schemas/expenses.ts
+++ src/database/schemas/expenses.ts
@@ -17,5 +17,6 @@ export const expensesTable = pgTable("expenses", {
 		.references(() => groupsTable.internal_id),
 	title: text("title").notNull(),
 	amount: decimal("amount", { precision: 14, scale: 3 }).notNull(),
+	split_mode: text("split_mode").notNull(),
 	created_at: timestamp("created_at").defaultNow().notNull(),
 });
